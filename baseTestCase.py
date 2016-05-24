ea15a64b8112e0144d7173a714867b8d320aa1cd
andluo <andluo90@gmail.com>
2016-05-24 15:20:18 +0800

    fix baseTestCase Bug and make sure the test can be runed.

--- baseTestCase.py
+++ baseTestCase.py
@@ -1,8 +1,7 @@
 #-*- coding:UTF-8 -*-
 __author__ = 'andluo'
 
-import unittest,pickle,time
-from selenium.common.exceptions import WebDriverException
+import pickle
 from factory import WebdriverFactory
 from unittest import  TestCase
 from testConfig import TestConfig
@@ -16,40 +15,34 @@ class BaseTestCase(TestCase):
     @classmethod
     def setUpClass(cls):
         "检测是否已登录，如果未登录，则登录后并且保存登录态"
-        try:
-            testUrl = cls.p.get('url')
-            user = cls.p.get('user')
-            pwd = cls.p.get('pwd')
 
-            cls.s.open(testUrl)
+        testUrl = cls.p.get('url')
+        user = cls.p.get('user')
+        pwd = cls.p.get('pwd')
 
-            with open("cookies.pkl","rb") as f:
-                cookies = pickle.load(f)
-            for cookie in cookies:
-                print cookie
-                cls.s.add_cookie(cookie)
+        cls.s.open(testUrl)
 
-            cls.s.open(testUrl+'/my/userCenter/')
+        with open("cookies.pkl","rb") as f:
+            cookies = pickle.load(f)
+        for cookie in cookies:
+            print cookie
+            print type(cookie)
+            cls.s.addCookie(cookie)
 
-            if cls.s.title == u'登录注册_果盘游戏':
-                #登录代码并保存cookie
-                cls.s.find_element_by_id("uname").send_keys(user)
-                cls.s.find_element_by_id("upwd").send_keys(pwd)
-                cls.s.find_element_by_css_selector('.loginBtn').click()
-                time.sleep(3)
-                if cls.s.title == u'个人中心首页_果盘游戏个人中心':
-                    with open('cookies.pkl','wb') as cookie_file:
-                        pickle.dump(cls.s.open_cookies(),cookie_file)
-                else:
-                    print 'login fail!'
+        cls.s.open(testUrl+'/my/userCenter/')
 
-            elif cls.s.title == u'个人中心首页_果盘游戏个人中心':
+        if cls.s.title == u'登录注册_果盘游戏':
+            #登录代码并保存cookie
+            cls.s.type("#uname",user)
+            cls.s.type("#upwd",pwd)
+            cls.s.click(".loginBtn")
+            cls.s.sleep(3)
+            if cls.s.title == u'个人中心首页_果盘游戏个人中心':
+                with open('cookies.pkl','wb') as cookie_file:
+                    pickle.dump(cls.s.getCookies(),cookie_file)
+            else:
+                print 'login fail!'
 
-                pass
-        except WebDriverException,e:
-            print e
-        except AssertionError,e:
-            print('bbbb')
     @classmethod
     def tearDownClass(cls):
         cls.s.close()
--- gpTestCase.py
+++ gpTestCase.py
@@ -7,33 +7,20 @@ from baseTestCase import  BaseTestCase
 
 class GPTestCase(BaseTestCase):
 
-    @unittest.skip('test')
     def test_addGroup(self):
         "test1"
         try:
-            self.driver.get('http://m.guopan.cn/my/userCenter/')
-            self.driver.find_element_by_css_selector('.userList .li0').click()
-            #self.driver.find_element_by_css_selector('abcdef').click()
-            self.driver.find_element_by_css_selector('.myGroupList .add').click()
-            self.driver.find_element_by_css_selector('.Agroup-list li:first .join_btn').click()
-            text = self.driver.find_element_by_css_selector('.Agroup-list li:first .has').text
+            s = self.s
+            s.open('http://m.guopan.cn/my/userCenter/')
+            s.click('.userList .li0')
+            s.click('.myGroupList .add')
+            s.click('.Agroup-list li:first .join_btn')
+            text = s.getText('.Agroup-list li:first .has')
+            self.assertEqual(u'已加入',text)
 
-            if text != u'已加入':
-                pass
         except WebDriverException,e:
-            raise WebDriverException(msg=e.msg,screen=self.driver.get_screenshot_as_file('./test.png'))
+            raise WebDriverException(msg=e.msg,screen=s.screenshot('./screenshot/test.png'))
 
-    @unittest.skip('test')
-    def test_addGroup2(self):
-        "test2"
-        self.driver.get('http://m.guopan.cn/my/userCenter/')
-        self.driver.find_element_by_css_selector('.userList .li0').click()
-        #self.driver.find_element_by_css_selector('abcdef').click()
-        self.driver.find_element_by_css_selector('.myGroupList .add').click()
-        self.driver.find_element_by_css_selector('.Agroup-list li:first .join_btn').click()
-        text = self.driver.find_element_by_css_selector('.Agroup-list li:first .has').text
-        if text != u'已加入':
-            pass
 
     def test_33(self):
         pass
